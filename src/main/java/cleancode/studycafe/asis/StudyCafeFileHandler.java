9349275d6e04d7815037789588ff154435dc946f
DESKTOP-36I1K9Q\Ekfmddlahgjark <dmstjd0214@naver.com>
2024-10-10 20:16:39 +0900

    [6-2] 리팩토링(1) - 중복 제거 , 메서드 추출

--- src/main/java/cleancode/studycafe/asis/StudyCafeFileHandler.java
+++ src/main/java/cleancode/studycafe/asis/StudyCafeFileHandler.java
@@ -1,4 +1,8 @@
-package cleancode.studycafe.asis.io;
+package cleancode.studycafe.asis;
+
+import cleancode.studycafe.asis.model.StudyCafeLockerPass;
+import cleancode.studycafe.asis.model.StudyCafePass;
+import cleancode.studycafe.asis.model.StudyCafePassType;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -6,12 +10,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import cleancode.studycafe.asis.model.StudyCafeLockerPass;
-import cleancode.studycafe.asis.model.StudyCafePass;
-import cleancode.studycafe.asis.model.StudyCafePassType;
-
 public class StudyCafeFileHandler {
-
     public List<StudyCafePass> readStudyCafePasses() {
         try {
             List<String> lines = Files.readAllLines(Paths.get("src/main/resources/cleancode/studycafe/pass-list.csv"));
@@ -29,7 +28,7 @@ public class StudyCafeFileHandler {
 
             return studyCafePasses;
         } catch (IOException e) {
-            throw new RuntimeException("파일을 읽는데 실패했습니다.", e);
+            throw new RuntimeException(e);
         }
     }
 
